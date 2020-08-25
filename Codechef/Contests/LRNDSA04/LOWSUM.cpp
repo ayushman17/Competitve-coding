ccc8138ee56ef644a1a906d4b00dcccb93ce2b9f
Devdatta Khoche <49261633+devdattakhoche@users.noreply.github.com>
2020-08-25 13:22:32 +0530

    Update LOWSUM.cpp

--- Codechef/Contests/LRNDSA04/LOWSUM.cpp
+++ Codechef/Contests/LRNDSA04/LOWSUM.cpp
@@ -1,31 +1,68 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll unsigned long long int
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	int tc; cin >> tc;
-	while (tc--) {
-		int k, q;
-		cin >> k >> q;
-		int m[k], s[k], sum[k * k];
-		for (int i = 0; i < k; i++) cin >> m[i];
-		for (int i = 0; i < k; i++) cin >> s[i];
-		int count = 0;
-		for (int w = 0; w < k; w++) {
-			for (int j = 0; j < k; j++) {
+void solve(int k,int q,vector<ll> &m,vector<ll> &s,vector<int> &query)
+{
+   sort(m.begin(),m.end());
+   sort(s.begin(),s.end());
+   vector<ll> c;
+   if(k<=100)
+   {
+       for(int i=0;i<k;i++)
+       {
+           for(int j=0;j<k;j++)
+            c.push_back(m[i]+s[j]);
+       }
+   }
+   else
+   {
+       ll maxi=m[100]+s[100];
+       for(int i=0;i<k;i++)
+       {
+           for(int j=0;j<k && m[i]+s[j]<maxi;j++)
+            c.push_back(m[i]+s[j]);
+       }
+   }
+   sort(c.begin(),c.end());
+   for(int i=0;i<q;i++)
+   {
+       cout<<c[query[i]-1]<<endl;
+   }
+}
 
-				sum[count] = m[w] + s[j];
-				// cout << "DBG----> " << " m " << m[w] << " s " << s[j] << " sum " << sum << endl;
-				count++;
-			}
-		}
-		sort(sum, sum + (k * k));
-		int temp;
-		for (int i = 0; i < q; i++) {
-			cin >> temp;
-			cout << sum[temp - 1] << " ";
-		}
-		cout << endl;
-	}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    int k,q;
+    vector<ll> m,s;
+    vector<int> query;
+    ll value;
+    while(t--)
+    {
+        cin>>k>>q;
+        for(int i=1;i<=k;i++)
+        {
+            cin>>value;
+            m.push_back(value);
+        }
+        for(int i=1;i<=k;i++)
+        {
+            cin>>value;
+            s.push_back(value);
+        }
+        for(int i=1;i<=q;i++)
+        {
+            cin>>value;
+            query.push_back(value);
+        }
+        solve(k,q,m,s,query);
+        m.clear();
+        s.clear();
+        query.clear();
+    }
+    return 0;
 }
