750f6d6563d60be9797351c471dc2b04770eb0b1
devdattakhoche <dkhoche70@gmail.com>
2020-08-25 13:17:12 +0530

    update

--- Codechef/Contests/LRNDSA04/LOWSUM.cpp
+++ Codechef/Contests/LRNDSA04/LOWSUM.cpp
@@ -4,5 +4,28 @@ using namespace std;
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	cout << "Hello from LOWSUM" << endl;
+	int tc; cin >> tc;
+	while (tc--) {
+		int k, q;
+		cin >> k >> q;
+		int m[k], s[k], sum[k * k];
+		for (int i = 0; i < k; i++) cin >> m[i];
+		for (int i = 0; i < k; i++) cin >> s[i];
+		int count = 0;
+		for (int w = 0; w < k; w++) {
+			for (int j = 0; j < k; j++) {
+
+				sum[count] = m[w] + s[j];
+				// cout << "DBG----> " << " m " << m[w] << " s " << s[j] << " sum " << sum << endl;
+				count++;
+			}
+		}
+		sort(sum, sum + (k * k));
+		int temp;
+		for (int i = 0; i < q; i++) {
+			cin >> temp;
+			cout << sum[temp - 1] << " ";
+		}
+		cout << endl;
+	}
 }
